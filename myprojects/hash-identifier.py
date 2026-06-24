9986058b597e859995726351377d601d42295141
MajedAshmela <39376069+MajedAshmela@users.noreply.github.com>
2026-06-24 22:50:27 +0200

    if descrypt

--- myprojects/hash-identifier.py
+++ myprojects/hash-identifier.py
@@ -15,4 +15,6 @@ def _is_hex(text):
 
 def _is_mysql5(text):
  return text.startswith("*") and len(text) == 41 and re.match(r"^[0-9A-F]+$", text[1:])
- /majed
+
+def _is_descrypt(text):
+    return bool(re.match(r"^[./0-9a-zA-Z]{13}$", text))
